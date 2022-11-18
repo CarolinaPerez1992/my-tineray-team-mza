44581916e7d5847c47a0109aa8322d42051ee83e
CarolinaPerez1992 <carolinabperez@outlook.com>
2022-11-18 01:22:56 -0300

    MT-F1-21 modificaciones filtros cruzados cn back

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -5,19 +5,14 @@ import FormCity from "../components/FormCity";
 import { useEffect } from "react";
 import axios from "axios";
 import {baseURL} from "../url"
+import NotElementFound from "../components/NotElementFound"
 
 export default function Cities() {
   let [ciudades, setCiudades] = useState([])
   let [ciudadesFiltradas, setCiudadesFiltradas] = useState([]);
+  let [checkboxes, setCheckBoxes] = useState([])
+  const searchId= useRef()
   
-  const America = useRef();
-  const Europa = useRef();
-  const Asia = useRef();
-  const Oceania = useRef();
-  const searchId = useRef();
-
-  let continentes = [America, Europa, Asia, Oceania];
-
   useEffect(() => {
     axios.get(`${baseURL}api/cities`)
         .then(response => setCiudades(response.data.data))
@@ -28,42 +23,58 @@ export default function Cities() {
 
   let checkContinent = [...new Set(ciudades.map((ciudad) => ciudad.continent))];
 
-  function filterCheckCards() {
-    let checkFiltered = filterCheck();
-    console.log(checkFiltered);
-    let searchFiltered = filterSearch(checkFiltered);
-    console.log(searchFiltered);
-    setCiudadesFiltradas(searchFiltered);
-    console.log(setCiudadesFiltradas);
-    localStorage.setItem("ciudadesFiltradas", JSON.stringify(searchFiltered));
+  // function filterCheckCards() {
+  //   let checkFiltered = filterCheck();
+  //   let searchFiltered = filterSearch(checkFiltered);
+  //   setCiudadesFiltradas(searchFiltered);
+  //   localStorage.setItem("ciudadesFiltradas", JSON.stringify(searchFiltered));
+  // }
+
+  function filterSearch(cityFil) {
+    // if (searchId.current.value !== "") {
+    //   let ciudadesFiltradas = array.filter((ciudad) =>
+    //     ciudad.name.toLowerCase().includes(searchId.current.value.toLowerCase())
+    //   );
+    //   return ciudadesFiltradas;
+    // } else {
+    //   return array;
+    // }
+    let check = filterCheck(cityFil)
+        let url = check.map( (continent) => `continent=${continent}`).join('&');
+        axios.get(`${baseURL}api/cities?${url}&name=${searchId.current.value}`)
+        .then(response => setCiudadesFiltradas(response.data.data))
   }
 
-  function filterCheck() {
+  function filterCheck(check) {
     let checks = [];
-    continentes
-      .filter((continente) => continente.current?.checked)
-      .map((continente) => checks.push(continente.current.value));
-    let ciudadesFiltradas = ciudades.filter((ciudad) =>
-      checks.includes(ciudad.continent)
-    );
-    console.log(checks.length);
-    if (checks.length === 0) {
-      console.log(ciudades);
-      return ciudades;
+    // continentes
+    //   .filter((continente) => continente.current?.checked)
+    //   .map((continente) => checks.push(continente.current.value));
+    // let ciudadesFiltradas = ciudades.filter((ciudad) =>
+    //   checks.includes(ciudad.continent)
+    // );
+    // console.log(checks.length);
+    if (check.target.checked) {
+      checks = [...checkboxes, check.target.value]
+      
+    } else{
+      checks = checkboxes.filter((checkbox) => checkbox !== check.target.value)
     }
-    return ciudadesFiltradas;
+    setCheckBoxes(checks)
+    return checks;
   }
+//   function checksFilter(event) {
+//     let arrayCheck = [];
+//     if (event.target.checked) {
+//       arrayCheck = [...checkboxes, event.target.value];
+//     } else {
+//       arrayCheck = checkboxes.filterSearch((e) => e !== event.target.value);
+//     }
+//     setCheckboxes(arrayCheck);
+//     return arrayCheck;
+// }
 
-  function filterSearch(array) {
-    if (searchId.current.value !== "") {
-      let ciudadesFiltradas = array.filter((ciudad) =>
-        ciudad.name.toLowerCase().includes(searchId.current.value.toLowerCase())
-      );
-      return ciudadesFiltradas;
-    } else {
-      return array;
-    }
-  }
+
   return (
     <>
       <div className="filter">
@@ -74,8 +85,8 @@ export default function Cities() {
                 id={index}
                 continent={continent}
                 valor={continent}
-                refId={continentes[index]}
-                fx={filterCheckCards}
+                // refId={continentes[index]}
+                fx={filterSearch}
               />
             );
           })}
@@ -85,14 +96,15 @@ export default function Cities() {
             type="text"
             placeholder="Search"
             ref={searchId}
-            onChange={filterCheckCards}
+            onChange={filterSearch}
           />
         </div>
       </div>
       <div className="cont-card">
-        {ciudadesFiltradas.map((cadaCity, id) => {
+        {ciudadesFiltradas.length > 0 ? (ciudadesFiltradas.map((cadaCity, id) => {
           return <CityCard datos={cadaCity} key={id} id={cadaCity._id}/>
-})}
+})):(<NotElementFound/>)
+}
       </div>
     </>
   );
