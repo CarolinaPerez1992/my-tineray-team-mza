7994496410f50605a8700b5862ac39885c16fb9c
Raulmoyano2970 <raulmoyano2970@gmail.com>
2022-11-18 10:00:22 -0300

    FIX-MT-02-24

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import SignIn from "./pages/SignIn";
 import NewCity from "./pages/NewCity";
 import NewHotel from "./pages/NewHotel";
 import AutoToTop from "./components/AutoToTop";
-import DetailCity from "./pages/DetailCity"
+import DetailCity from "./pages/DetailCity";
 import DetailHotel from "./pages/DetailHotel";
 
 function App() {
@@ -28,9 +28,8 @@ function App() {
           <Route path="/signin" element={<SignIn />}></Route>
           <Route path="/newcity" element={<NewCity />}></Route>
           <Route path="/newhotel" element={<NewHotel />}></Route>
-          <Route path="/detailcity/:id" element={<DetailCity/>}></Route>
-          <Route path="/detailhotel/:id" element={<DetailHotel/>}></Route>
-
+          <Route path="/detailcity/:id" element={<DetailCity />}></Route>
+          <Route path="/detailhotel/:id" element={<DetailHotel />}></Route>
         </Routes>
       </Layout>
     </BrowserRouter>
